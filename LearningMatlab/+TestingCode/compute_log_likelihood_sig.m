176a18aabbfe8f54e0de2d009cef9591482c59d5
ankani <ankanichattoraj91@gmail.com>
2018-04-05 14:00:37 -0400

    Removing +TestingCode files that are redundant with +LearningParams files

--- LearningMatlab/+TestingCode/compute_log_likelihood_sig.m
+++
@@ -1,5 +0,0 @@
-function log_prob_sig = compute_log_likelihood_sig(prms,stim,z_hidden)
-% log_prob_sig = log(1.0/(2.0^(prms.pix*prms.pix/2.0)*pi^(prms.pix*prms.pix/2.0)*prms.sigma_stim^(prms.pix*prms.pix))) - ((1.0/(2.0*prms.sigma_stim^2)) * norm(stim(:) - prms.G*z_hidden(:))^2) + log(prms.prior)*sum(z_hidden) + log(1-prms.prior)*sum((1-z_hidden)); 
-log_prob_sig = log(1.0/prms.sigma_stim^(prms.pix*prms.pix)) - ((1.0/(2.0*prms.sigma_stim^2)) * norm(stim(:) - prms.G*z_hidden(:))^2); 
-
-end
