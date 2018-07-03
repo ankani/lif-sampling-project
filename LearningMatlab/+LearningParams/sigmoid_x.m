0be2b1f52cbeb9e32012603bdfe0ab98dbcbc376
wrongu <rlange@ur.rochester.edu>
2018-07-03 13:08:07 -0400

    sigmoid_x clipping to [eps, 1-eps] for stability in expectations

--- LearningMatlab/+LearningParams/sigmoid_x.m
+++ LearningMatlab/+LearningParams/sigmoid_x.m
@@ -1,4 +1,10 @@
 % function to compute the sigmoid of any value
 function result = sigmoid_x(x)
-result = (1.0)/(1.0 + exp(-x));
+if x > 36
+    result = 1-eps;
+elseif x < -36
+    result = eps;
+else
+    result = (1.0)/(1.0 + exp(-x));
+end
 end
